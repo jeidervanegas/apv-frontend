008fa232cc2f0338daa1e600bf6118d99a71ad2f
Jeider gomez <jeidergomezv@gmail.com>
2023-12-21 13:45:38 -0500

    feat:Cuarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import { ProtectedRoute } from './layout/ProtectedRoute'
 import { DanagePatients } from './pages/DanagePatients'
 import { EditProfile } from './pages/EditProfile'
 import { ChangePassword } from './pages/ChangePassword'
-axios.defaults.baseURL = 'http://localhost:3026/api'
+// axios.defaults.baseURL = 'http://localhost:3026/api'
+axios.defaults.baseURL = 'https://apv-backend-dev-dtah.4.us-1.fl0.io/api'
 
 
 function App() {
